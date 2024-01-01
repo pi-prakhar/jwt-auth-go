d4699a4f98c07e1837eb2001a3299997a8606e4f
Prakhar Awasthi <pawasthi961@gmail.com>
2024-01-01 18:03:33 +0530

    base project

---
+++ helpers/authHelper.go
@@ -0,0 +1,31 @@
+package helper
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("userType")
+	err = nil
+	if userType != role {
+		err = errors.New("Unauthorized to access this resourcce")
+		return err
+	}
+	return err
+}
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("userType")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+	err = CheckUserType(c, userType)
+	return err
+
+}
